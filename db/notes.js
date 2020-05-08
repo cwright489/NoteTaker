0dd9e817b10007b53d812b420d9bf7962e3386cf
Chance Wright <cwright489@gmail.com>
2020-05-07 19:12:55 -0600

    fixing routes

--- db/notes.js
+++ db/notes.js
@@ -1,60 +1,52 @@
 const util = require("util");
 const fs = require("fs");
 
-const readFileAsync = util.promisify(fs.readFile);
+const readFileAsyn = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-class Store {
-  constructor() {
-    this.lastId = 0;
-  }
 
-  read() {
-    return readFileAsync("db/db.json", "utf8");
-  }
-
-  write(note) {
-    return writeFileAsync("db/db.json", JSON.stringify(note));
-  }
-
-  getNotes() {
-    return this.read().then(notes => {
-      let parsedNotes;
-
-      // If notes isn't an array or can't be turned into one, send back a new empty array
-      try {
-        parsedNotes = [].concat(JSON.parse(notes));
-      } catch (err) {
-        parsedNotes = [];
-      }
-
-      return parsedNotes;
-    });
-  }
-
-  addNote(note) {
-    const { title, text } = note;
-
-    if (!title || !text) {
-      throw new Error("Note 'title' and 'text' cannot be blank");
+class Notes {
+    constructor() {
+        this.idDum = 0;
     }
+    read() {
+        return readFileAsyn("db/db.json", "utf8");
 
-    // Increment `this.lastId` and assign it to `newNote.id`
-    const newNote = { title, text, id: ++this.lastId };
+    }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
+    getNotes() {
+        console.log("get notes")
+        return this.read().then(notes => {
+            console.log(notes)
+            let notesArray;
+            try {
+                notesArray = [].concat(JSON.parse(notes));
+            }
+            catch (err) {
+                notesArray = [];
+            }
+            return notesArray;
+        })
 
-    // Get all notes, add the new note, write all the updated notes, return the newNote
-    return this.getNotes()
-      .then(notes => [...notes, newNote])
-      .then(updatedNotes => this.write(updatedNotes))
-      .then(() => newNote);
-  }
+    }
+    addNotes(note) {
+        console.log("add notes");
+        const { title, text } = note;
+        const newNote = { title, text, id: ++this.idDum }
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updateNotes => this.write(updateNotes))
+            .then(() => newNote)
 
-  removeNote(id) {
-    // Get all notes, remove the note with the given id, write the filtered notes
-    return this.getNotes()
-      .then(notes => notes.filter(note => note.id !== parseInt(id)))
-      .then(filteredNotes => this.write(filteredNotes));
-  }
+    }
+    removeNote(id) {
+        console.log("remove notes");
+        return this.getNotes()
+            .then(notes => notes.filter(note => note.id !== parseInt(id)))
+            .then(updatedNotes => this.write(updatedNotes))
+    }
 }
 
-module.exports = new Store();
+module.exports = new Notes();
