ef15a3890e31d76238eec2e27dcd552b4b24fbca
Chance Wright <cwright489@gmail.com>
2020-05-17 17:40:12 -0600

    heroku

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,24 +1,24 @@
 const router = require("express").Router();
-const store = require("../db/notes");
+const notes = require("../db/notes");
 
 // GET "/api/notes" responds with all notes from the database
 router.get("/notes", function(req, res) {
-  store
+  notes
     .getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
 
 router.post("/notes", (req, res) => {
-  store
-    .addNote(req.body)
+  notes
+    .addNotes(req.body)
     .then((note) => res.json(note))
     .catch(err => res.status(500).json(err));
 });
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
 router.delete("/notes/:id", function(req, res) {
-  store
+  notes
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch(err => res.status(500).json(err));
